c9666a22b3ba870b929185e6a3f64218e7aaf62f
Sam Lam <samlam@varadise.cloud>
2025-11-07 11:01:12 +0800

    Add essentials

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,20 @@
-import { Text, View } from "react-native";
+import { ColorPicker, Host } from '@expo/ui/swift-ui';
+import { useState } from 'react';
+import { useWindowDimensions, View } from 'react-native';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Host style={{ height: 400, width: '100%' }}>
+        <MyBottomSheet />
+      </Host>
     </View>
   );
+}
+
+function MyBottomSheet() {
+  const [color, setColor] = useState<string>('#FF0000');
+  const { width } = useWindowDimensions();
+
+  return <ColorPicker label="Select a color" selection={color} onValueChanged={setColor} />;
 }
