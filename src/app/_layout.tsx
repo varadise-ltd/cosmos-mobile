05f94a58a3d48400cc8a8f52c5ca33e2bbe202e1
Sam <samlam@varadise.cloud>
2025-11-10 17:31:36 +0800

    Move files correctly

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,6 @@
 import { Stack } from 'expo-router';
-import { ThemeProvider } from '../components/ThemeProvider';
-import '../tokens/global.css';
+import '../../global.css';
 
 export default function RootLayout() {
-  return (
-    <ThemeProvider>
-      <Stack />
-    </ThemeProvider>
-  );
+  return <Stack />;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /* globals __dirname */
 
 const path = require('path');
-const cssVariablePlugin = require('./src/plugins/css-variable-plugin.js');
+const cssVariablePlugin = require('./plugins/css-variable-plugin.js');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -17,21 +17,14 @@ module.exports = {
       variables: {
         fontFamily: ['family-hand', 'family-base'],
       },
-      // Auto-scan CSS file for variables
       autoScan: true,
-      cssFile: path.join(__dirname, 'src/tokens/global.css'),
+      cssFile: path.join(__dirname, './global.css'),
       patterns: {
-        // Auto-generate color utilities for variables starting with '--color'
         colors: '^color-',
         borderRadius: '^radius-',
-        // // Auto-generate spacing utilities for size and sizing variables
         spacing: '^spacing-',
-        // // Auto-generate font utilities based on theme files
-        // fontWeight: '^(weight-(reg|med|smb|bold)|component-.*-weight)$',
-        // fontSize: '^component-.*-font-.*-size$',
-        // fontFamily: '^component-.*-font-.*-family$',
       },
-      debug: true, // Set to true to see debug logs
+      debug: false, // Set to true to see debug logs
     }),
   ],
 };
